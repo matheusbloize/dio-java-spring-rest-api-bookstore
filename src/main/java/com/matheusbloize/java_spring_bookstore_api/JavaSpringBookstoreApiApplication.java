8e96c51f68d589fbdc4f30e8c40484c43fb73bec
Matheus Bloize <mbloizepd@gmail.com>
2025-02-19 15:46:33 -0300

    feat: start project

---
+++ src/main/java/com/matheusbloize/java_spring_bookstore_api/JavaSpringBookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheusbloize.java_spring_bookstore_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBookstoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusbloize/java_spring_bookstore_api/JavaSpringBookstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusbloize.java_spring_bookstore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBookstoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
