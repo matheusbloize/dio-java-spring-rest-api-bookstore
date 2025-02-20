0144e4922450b8c0ff0b1d408d0445ded49e62ef
Matheus Bloize <mbloizepd@gmail.com>
2025-02-20 16:46:22 -0300

    fix: cors

--- src/main/java/com/matheusbloize/java_spring_bookstore_api/JavaSpringBookstoreApiApplication.java
+++ src/main/java/com/matheusbloize/java_spring_bookstore_api/JavaSpringBookstoreApiApplication.java
@@ -3,6 +3,10 @@ package com.matheusbloize.java_spring_bookstore_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @SpringBootApplication
 public class JavaSpringBookstoreApiApplication {
 
